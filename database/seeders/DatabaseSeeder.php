bc3755f863ea6c1dc20a6a2e538e64aa4502b876
Veyro1975 <farrelherdiyan@gmail.com>
2026-06-14 00:01:42 +0700

    menambahkan akun untuk admin dan test seller

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,34 @@ class DatabaseSeeder extends Seeder
 
     public function run(): void
     {
+
+        $this->call([
+            RolePermissionSeeder::class,
+        ]);
+
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@travel.com'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('password123'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $admin->assignRole('Admin');
+
+        $seller = User::firstOrCreate(
+            ['email' => 'seller@travel.com'],
+            [
+                'name' => 'Seller1',
+                'password' => Hash::make('password123'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $seller->assignRole('Seller');
+
+
         $this->call([
             DestinationSeeder::class,
             ProductSeeder::class,
